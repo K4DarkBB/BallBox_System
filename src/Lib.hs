db261ad8c2a34c0856c2b19f10deae21459f7afc
Kasshi_K <kasshi.4259.b.b@gmail.com>
2021-04-30 09:48:45 +0900

    Add lifegame

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,25 @@
 module Lib where
 
+import Data.Bits
+import Data.Word
+
 data Ball = None | Exist | Will
 
 instance Show Ball where
-  show None="."
-  show Exist="o"
-  show Will="w"
+  show None = "."
+  show Exist = "o"
+  show Will = "w"
 
-update :: [Ball] -> [Ball]
-update [] = []
-update [a] = [a]
-update (Exist : x) = None : update (take c x <> [Will] <> drop (c + 1) x)
+boxUpdate :: [Ball] -> [Ball]
+boxUpdate [] = []
+boxUpdate [a] = [a]
+boxUpdate (Exist : x) = None : boxUpdate (take c x <> [Will] <> drop (c + 1) x)
   where
     getExCount [] = 0
     getExCount (None : _) = 0
     getExCount (_ : x) = 1 + getExCount x
     c = getExCount x
-update (e : x) = e : update x
+boxUpdate (e : x) = e : boxUpdate x
 
 finalize [] = [None]
 finalize (Will : x) = Exist : finalize x
@@ -32,3 +35,32 @@ toBox :: String -> [Ball]
 toBox [] = []
 toBox ('o' : s) = Exist : toBox s
 toBox (_ : s) = None : toBox s
+
+----------------------------------------
+
+data LifeGame = LifeGame Integer Int Int
+
+pixel :: (Num a, Bits a) => LifeGame -> Int -> Int -> a
+pixel (LifeGame bits width height) x y = if testBitDefault bits $ u * width + v then 1 else 0
+  where
+    u = x .&. finiteBitSize width
+    v = y .&. finiteBitSize height
+
+--789 46 123.
+neighbormoore :: (Num a, Bits a) => LifeGame -> Int -> Int -> [a]
+neighbormoore lg@(LifeGame _ width height) x y =
+  [p (x -1) (y -1), p x (y -1), p (x + 1) (y -1), p (x -1) y, p x (y + 1), p (x + 1) (y -1), p (x + 1) y, p (x + 1) (y + 1)]
+  where
+    p = pixel lg
+
+sumBits :: (Num a, Bits a) => [a] -> (a, a)
+sumBits x = (0, 0)
+  where
+    p = (x !!)
+    xab = p 0 .&. p 1
+    xcd = p 2 .&. p 3
+    xef = p 4 .&. p 5
+    xgh = p 6 .&. p 7
+
+lifeUpdate :: Bits a => Integer -> a -> Integer
+lifeUpdate 0 _ = 0
